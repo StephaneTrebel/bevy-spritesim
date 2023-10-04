f79f90519982c8ab01aa1fad669c60ff9da45f71
Stéphane TRÉBEL (Perso) <stephane.trebel@gmail.com>
2023-10-04 12:00:47 +0200

    Add map generation first iteration

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,55 @@
 use bevy::{prelude::*, window::*};
+use rand::{rngs::StdRng, Rng, SeedableRng};
+
+enum Color {
+    Red,
+    Green,
+    Blue,
+}
+
+struct Tile {
+    coordinates: Vec3,
+    color: Color,
+}
+
+fn build_map(width: i32, height: i32, rng: &mut StdRng) -> Vec<Tile> {
+    let mut map = Vec::new();
+    for w in 0..width {
+        for h in 0..height {
+            let test = rng.gen_range(0..3);
+            map.push(Tile {
+                coordinates: Vec3::from(((w as f32) * 32., (h as f32) * 32., 0.)),
+                color: match test {
+                    1 => Color::Red,
+                    2 => Color::Blue,
+                    _ => Color::Green,
+                },
+            });
+        }
+    }
+    return map;
+}
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let mut rng = StdRng::seed_from_u64(19878367467713);
+    let map = build_map(10, 10, &mut rng);
     commands.spawn(Camera2dBundle::default());
 
-    commands.spawn(SpriteBundle {
-        texture: asset_server.load("sprites/terrain/red.png"),
-        transform: Transform::from_xyz(100., 0., 0.),
-        ..default()
-    });
-    commands.spawn(SpriteBundle {
-        texture: asset_server.load("sprites/terrain/green.png"),
-        transform: Transform::from_xyz(50., 50., 0.),
-        ..default()
-    });
-    commands.spawn(SpriteBundle {
-        texture: asset_server.load("sprites/terrain/blue.png"),
-        transform: Transform::from_xyz(0., 0., 0.),
-        ..default()
-    });
+    for item in map {
+        commands.spawn(SpriteBundle {
+            texture: match item.color {
+                Color::Red => asset_server.load("sprites/terrain/red.png"),
+                Color::Blue => asset_server.load("sprites/terrain/blue.png"),
+                Color::Green => asset_server.load("sprites/terrain/green.png"),
+            },
+            transform: Transform::from_xyz(
+                item.coordinates.x as f32,
+                item.coordinates.y as f32,
+                item.coordinates.z as f32,
+            ),
+            ..default()
+        });
+    }
 }
 
 pub struct GamePlugin;
