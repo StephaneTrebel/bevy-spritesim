0a93b234ed9970855317fe89d09495852484d48e
Stéphane TRÉBEL (Perso) <stephane.trebel@gmail.com>
2023-10-04 12:00:49 +0200

    Improve tile patch generation

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
 use bevy::{math::vec2, prelude::*, render::camera::ScalingMode, window::*};
 use bevy_pancam::{PanCam, PanCamPlugin};
-use noisy_bevy::fbm_simplex_2d;
+use noisy_bevy::{fbm_simplex_2d, simplex_noise_2d};
 // use rand::{rngs::StdRng, Rng, SeedableRng};
 
-const WINDOW_PHYSICAL_WIDTH: f32 = 1280.;
-const WINDOW_PHYSICAL_HEIGHT: f32 = 1280.;
-const WINDOW_SCALE_FACTOR: f64 = 2.0;
-const OFFSET_WIDTH: f32 =
-    WINDOW_PHYSICAL_WIDTH / (2. * WINDOW_SCALE_FACTOR as f32) - SPRITE_SIZE / 2.;
-const OFFSET_HEIGHT: f32 =
-    WINDOW_PHYSICAL_HEIGHT / (2. * WINDOW_SCALE_FACTOR as f32) - SPRITE_SIZE / 2.;
+const WINDOW_PHYSICAL_WIDTH: f32 = 1280.; // In pixels
+const WINDOW_PHYSICAL_HEIGHT: f32 = 1280.; // In pixels
+const WINDOW_SCALE_FACTOR: f64 = 2.0; // How much tiles are streched out in the beginning
 const SPRITE_SIZE: f32 = 32.;
+const MAP_WIDTH: usize = 100;
+const MAP_HEIGHT: usize = 100;
 
 enum Kind {
     Plain,
@@ -20,41 +18,58 @@ enum Kind {
 
 struct Tile {
     kind: Kind,
+    transform: Transform,
 }
 
 fn build_map_v2(width: i32, height: i32) -> Vec<Tile> {
     let mut map = Vec::new();
 
     // Init with Ocean tiles
-    for _ in 0..width * height {
-        map.push(Tile { kind: Kind::Ocean });
+    for w in 0..width {
+        for h in 0..height {
+            map.push(Tile {
+                kind: Kind::Ocean,
+                transform: Transform::from_xyz(
+                    (w as f32) * SPRITE_SIZE
+                        - (WINDOW_PHYSICAL_WIDTH / (2. * WINDOW_SCALE_FACTOR as f32)
+                            - SPRITE_SIZE / 2.),
+                    (h as f32) * SPRITE_SIZE
+                        - (WINDOW_PHYSICAL_HEIGHT / (2. * WINDOW_SCALE_FACTOR as f32)
+                            - SPRITE_SIZE / 2.),
+                    0.,
+                ),
+            });
+        }
     }
 
     // Setup constants for noisy_bevy
-    const FREQUENCY_SCALE: f32 = 0.05;
-    const AMPLITUDE_SCALE: f32 = 4.0;
-    const RADIUS: f32 = 5.;
-    const OCTAVES: usize = 3;
-    const LACUNARITY: f32 = 2.;
-    const GAIN: f32 = 0.5;
+    const FREQUENCY_SCALE: f32 = 0.2;
+    const AMPLITUDE_SCALE: f32 = 8.0;
+    const RADIUS: f32 = 30.;
 
     // Generate a patch of Plain in the middle
-    let grid = 2 * RADIUS as i32;
-    for x in 0..=grid {
-        print!("x: {} ", x);
-        for y in 0..=grid {
-            print!("y: {} ", y);
-            let p = vec2(x as f32, y as f32);
+    let grid_half_size = RADIUS as i32 + 1;
+    for w in -grid_half_size..=grid_half_size {
+        for h in -grid_half_size..=grid_half_size {
+            let p = vec2(w as f32, h as f32);
 
             // Compute noise offset (so the "blob" shape the patch will have)
-            let offset =
-                fbm_simplex_2d(p * FREQUENCY_SCALE, OCTAVES, LACUNARITY, GAIN) * AMPLITUDE_SCALE;
+            let offset = simplex_noise_2d(p * FREQUENCY_SCALE) * AMPLITUDE_SCALE;
 
             // Height will serve, with a cutoff, as sizing the resulting patch
-            let height = ((x * x + y * y) as f32).sqrt() - (RADIUS + offset);
-            println!("height: {}", height);
-            if height > 5. {
-                map[(x * width + y) as usize].kind = Kind::Plain;
+            let height = RADIUS + offset - ((w * w + h * h) as f32).sqrt();
+            let min_height = -1.;
+            dbg!(w, h, height, offset);
+
+            if height > min_height {
+                map.push(Tile {
+                    kind: Kind::Plain,
+                    transform: Transform::from_translation(Vec3::new(
+                        (w as f32) * SPRITE_SIZE + WINDOW_PHYSICAL_WIDTH,
+                        (h as f32) * SPRITE_SIZE + WINDOW_PHYSICAL_HEIGHT,
+                        1.,
+                    )),
+                });
             }
         }
     }
@@ -71,8 +86,6 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     // let mut rng = StdRng::seed_from_u64(seed);
 
     // Map generation
-    const MAP_WIDTH: usize = 20;
-    const MAP_HEIGHT: usize = 20;
     let map = build_map_v2(MAP_WIDTH as i32, MAP_HEIGHT as i32);
 
     // Configure Camera that can be panned and zoomed with the mouse
@@ -81,27 +94,19 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn((cam, PanCam::default()));
 
     // Load the sprites
+    // let forest_sprite_handle = asset_server.load("sprites/terrain/forest.png");
     let ocean_sprite_handle = asset_server.load("sprites/terrain/ocean.png");
     let plain_sprite_handle = asset_server.load("sprites/terrain/plain.png");
 
     // Display the sprites
-    for (index, item) in map.into_iter().enumerate() {
+    for (_, item) in map.into_iter().enumerate() {
         commands.spawn(SpriteBundle {
             texture: match item.kind {
-                // Kind::Forest => asset_server.load("sprites/terrain/forest.png"),
+                // Kind::Forest => forest_sprite_handle.clone(),
                 Kind::Ocean => ocean_sprite_handle.clone(),
                 Kind::Plain => plain_sprite_handle.clone(),
             },
-            transform: {
-                let w = index / MAP_WIDTH;
-                let h = index - (w * MAP_WIDTH);
-                println!("index = {}, w = {}, h = {}", index, w, h);
-                Transform::from_xyz(
-                    (w as f32) * SPRITE_SIZE - OFFSET_WIDTH,
-                    (h as f32) * SPRITE_SIZE - OFFSET_HEIGHT,
-                    0.,
-                )
-            },
+            transform: item.transform,
             ..default()
         });
     }
