a225643bc339de834a08263533df75b911f57035
Stéphane TRÉBEL (Perso) <stephane.trebel@gmail.com>
2023-10-04 17:02:04 +0200

    Improve patch generation by allowing a wider params range

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::u32::MAX as MAX_u32;
 use std::u64::MAX as MAX_u64;
+use std::{ops::Range, u32::MAX as MAX_u32};
 
 use bevy::{math::vec2, prelude::*, render::camera::ScalingMode, utils::HashMap, window::*};
 use bevy_pancam::{PanCam, PanCamPlugin};
@@ -12,10 +12,6 @@ const WINDOW_SCALE_FACTOR: f64 = 2.0; // How much tiles are streched out in the 
 const SPRITE_SIZE: f32 = 32.;
 const MAP_WIDTH: i32 = 100;
 const MAP_HEIGHT: i32 = 100;
-
-// Setup constants for noisy_bevy
-const BASE_FREQUENCY_SCALE: f32 = 0.05;
-const BASE_AMPLITUDE_SCALE: f32 = 4.0;
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 enum Kind {
@@ -55,29 +51,25 @@ fn generate_patch(
             if *kind == Kind::Forest {
                 dbg!(height);
             }
-            let min_height = -1.;
+            let min_height = 0.;
 
             let key = (coordinates.0 + w, coordinates.1 + h);
-            // Only replace tile when necessary (for instance, Forest tiles can only be placed on Plains)
-            let replace = !(*kind == Kind::Forest && map.get(&key).unwrap().kind != Kind::Plain);
-            if replace
-                && height > min_height
-                && key.0 > 0
-                && key.1 > 0
-                && key.0 < MAP_WIDTH
-                && key.1 < MAP_HEIGHT
-            {
-                map.insert(
-                    key,
-                    Tile {
-                        kind: kind.clone(),
-                        transform: Transform::from_translation(Vec3::new(
-                            (coordinates.0 + w) as f32 * SPRITE_SIZE,
-                            (coordinates.1 + h) as f32 * SPRITE_SIZE,
-                            1., // z-index
-                        )),
-                    },
-                );
+            if key.0 > 0 && key.1 > 0 && key.0 < MAP_WIDTH && key.1 < MAP_HEIGHT {
+                // Only replace tile when necessary (for instance, Forest tiles can only be placed on Plains)
+                let replace = *kind != Kind::Forest || map.get(&key).unwrap().kind == Kind::Plain;
+                if replace && height > min_height {
+                    map.insert(
+                        key,
+                        Tile {
+                            kind: kind.clone(),
+                            transform: Transform::from_translation(Vec3::new(
+                                (coordinates.0 + w) as f32 * SPRITE_SIZE,
+                                (coordinates.1 + h) as f32 * SPRITE_SIZE,
+                                1., // z-index
+                            )),
+                        },
+                    );
+                }
             }
         }
     }
@@ -88,16 +80,17 @@ fn generate_multiple_patches(
     mut map: &mut Map,
     kind: Kind,
     count: i32,
-    min_radius: i32,
-    max_radius: i32,
-    delta_frequency_scale: f32,
-    delta_amplitude_scale: f32,
+    radius_range: Range<i32>,
+    frequency_range: Range<f32>,
+    amplitude_range: Range<f32>,
 ) {
+    // Positions patches centers on the map
+    // (kinda equidistant, but with random variations)
     let max_offset = 5;
-    let mut points: Vec<(i32, i32)> = Vec::new();
+    let mut patch_centers: Vec<(i32, i32)> = Vec::new();
     for w in 1..count {
         for h in 1..count {
-            points.push((
+            patch_centers.push((
                 pseudo_rng_instance.gen_range(-max_offset..=max_offset)
                     + MAP_WIDTH as i32 * w / count,
                 pseudo_rng_instance.gen_range(-max_offset..=max_offset)
@@ -105,21 +98,17 @@ fn generate_multiple_patches(
             ));
         }
     }
-    for coordinates in points {
+
+    // Main generation process
+    for coordinates in patch_centers {
         generate_patch(
             pseudo_rng_instance.gen_range(0..MAX_u32) as f32,
             &mut map,
             &kind,
             coordinates,
-            pseudo_rng_instance.gen_range(min_radius..max_radius) as f32,
-            pseudo_rng_instance.gen_range(
-                BASE_FREQUENCY_SCALE - delta_frequency_scale
-                    ..BASE_FREQUENCY_SCALE + delta_frequency_scale,
-            ),
-            pseudo_rng_instance.gen_range(
-                BASE_AMPLITUDE_SCALE - delta_amplitude_scale
-                    ..BASE_AMPLITUDE_SCALE + delta_amplitude_scale,
-            ),
+            pseudo_rng_instance.gen_range(radius_range.clone()) as f32,
+            pseudo_rng_instance.gen_range(frequency_range.clone()),
+            pseudo_rng_instance.gen_range(amplitude_range.clone()),
         );
     }
 }
@@ -129,7 +118,7 @@ fn build_map(mut pseudo_rng_instance: &mut StdRng) -> Map {
     dbg!(map_seed);
     let mut map: Map = HashMap::new();
 
-    // Init with Ocean tiles
+    // Initialize the whole map with Ocean tiles
     {
         for w in 0..MAP_WIDTH {
             for h in 0..MAP_HEIGHT {
@@ -149,16 +138,15 @@ fn build_map(mut pseudo_rng_instance: &mut StdRng) -> Map {
     }
 
     // Generate patches of Plain to serve as a main continent
-    // (but with an irregular shape)
+    // (but with an irregular shape by overlapping them)
     generate_multiple_patches(
         &mut pseudo_rng_instance,
         &mut map,
         Kind::Plain,
         6,
-        2,
-        20,
-        0.01,
-        0.4,
+        2..20,
+        0.04..0.06,
+        3.60..4.40,
     );
 
     // Generate randow patches of Forests
@@ -166,11 +154,10 @@ fn build_map(mut pseudo_rng_instance: &mut StdRng) -> Map {
         &mut pseudo_rng_instance,
         &mut map,
         Kind::Forest,
-        6,
-        1,
-        5,
-        0.05,
-        0.8,
+        15,
+        1..3,
+        0.05..1.0,
+        3.60..4.40,
     );
 
     return map;
