52a493ff4212294bb7365859d2673c4e15173f2b
Stéphane TRÉBEL (Perso) <stephane.trebel@gmail.com>
2026-06-05 21:22:49 +0200

    Fix Village creation

--- src/plugins/keyboard/plugin.rs
+++ src/plugins/keyboard/plugin.rs
@@ -12,7 +12,7 @@ pub struct KeyboardPlugin;
 impl Plugin for KeyboardPlugin {
     fn build(&self, app: &mut App) {
         app
-            // .add_systems(PreUpdate, handle_input)
+            .add_systems(PreUpdate, handle_input)
             .add_systems(OnEnter(AppState::ReadyToDraw), draw_village);
     }
 }
@@ -31,10 +31,11 @@ pub fn draw_village(mut commands: Commands, atlas: Res<SpriteAtlas>) {
 fn handle_input(
     mut commands: Commands,
     keyboard_input: Res<ButtonInput<KeyCode>>,
-    unit: Single<(Entity, &Transform), With<Unit>>,
+    unit: Single<(Entity, &Transform), (With<Unit>, Without<Village>)>,
     mut village: Single<(&mut Visibility, &mut Transform), With<Village>>,
     mut next_state: ResMut<NextState<AppState>>,
 ) {
+    info!("Key pressed !");
     let (entity, transform) = *unit;
     // B for "build village"
     if keyboard_input.just_pressed(KeyCode::KeyQ) {
